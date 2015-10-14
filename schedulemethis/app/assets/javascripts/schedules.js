45223046600262d2cf69a74a6fec0f85a1402803
Graham Schilling <gschillling@Grahams-MBP.hsd1.pa.comcast.net>
2015-10-14 14:14:16 -0400

    modal styling

--- schedulemethis/app/assets/javascripts/schedules.js
+++ schedulemethis/app/assets/javascripts/schedules.js
@@ -31,7 +31,7 @@ ready = function() {
     // Handling Modals Interactions for Full Calendar
       $('.modal-trigger').leanModal({
           dismissible: true, // Modal can be dismissed by clicking outside of the modal
-          opacity: .9, // Opacity of modal background
+          opacity: .6, // Opacity of modal background
           in_duration: 500, // Transition in duration
           out_duration: 200, // Transition out duration
         }
