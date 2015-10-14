0c8280237cc17af783b82ab69e4ae53ca891b81c
Graham Schilling <gschillling@Grahams-MBP.hsd1.pa.comcast.net>
2015-10-14 14:07:22 -0400

    modal styling

--- schedulemethis/app/assets/javascripts/schedules.js
+++ schedulemethis/app/assets/javascripts/schedules.js
@@ -29,7 +29,14 @@ ready = function() {
     }); //closes full Calendar
 
     // Handling Modals Interactions for Full Calendar
-    $('.modal-trigger').leanModal();
+      $('.modal-trigger').leanModal({
+          dismissible: true, // Modal can be dismissed by clicking outside of the modal
+          opacity: .9, // Opacity of modal background
+          in_duration: 500, // Transition in duration
+          out_duration: 200, // Transition out duration
+        }
+      );
+      
     $('.actions2').click(function() {
         $("#modal1").closeModal();
     }); //closes action2
